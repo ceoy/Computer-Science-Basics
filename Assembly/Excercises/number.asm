d141b0122e77404a72220ff32da212c761b67831
Tim Jahn <ceoymw@gmail.com>
2017-10-17 21:29:03 +0200

    shitty assembly

--- Assembly/Excercises/number.asm
+++ Assembly/Excercises/number.asm
@@ -1,31 +1,44 @@
 section .data
-    theString db "String 10 blbalb$"
+    inputString db "S1tri2ng  b3lbalb$"
+    outputString db "               ", 10
+    
 section .text
     global _start
 _start:
-    mov eax, [theString] ; put start of string into eax
-    mov ebx, 0 ; initialise the number
+    mov eax, inputString ; put start of string into eax
+    mov ebx, outputString ; put the start of the outputstrig into ebx
+    mov edx, 0 ; string length
 loop:
-    cmp eax, '$' ; check if we reached the end ($)
-    je finished  ; equals, so we are done
-    cmp eax, 48  ; 0 in ascii
+    inc edx
+    cmp byte [eax], '$' ; check if we reached the end ($)
+    jz finished  ; equals, so we are done
+    cmp byte [eax], '0'  ; 0 in ascii
     jb continueLoop ;
-    cmp eax, 57  ; 9 in ascii
+    cmp byte [eax], '9'  ; 9 in ascii
     ja continueLoop
-    mov ebx, ebx ;
-    ; increment positions
+    
+isNumber:   
+    mov dl, byte [eax]
+    mov [ebx], dl     ; move asci number to the new position
     inc ebx
     inc eax
-    
     jmp loop
 
 continueLoop:
     inc eax
+    inc ebx
     jmp loop
     
-isNumber:
-    
 finished:
+    inc ebx ; increase position by 1
+    
+    mov eax, 4 ; sys write
+    mov ebx, 1 ; standard output
+    mov ecx, outputString ; start string
+    
+    int 80H
+    
+    mov eax, 4 ; the stupid shit
     mov eax, 1 ; code for exiting
     mov ebx, 0 ; return code
     int 80H    ; kernel call

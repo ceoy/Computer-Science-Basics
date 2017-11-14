bf6e407e94f22611d8b78f8e670fd5b98e7dcfc8
Tim Jah <ceoymw@gmail.com>
2017-11-14 17:54:29 +0100

    adds c

---
+++ C/cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C.dir/main.c.obj"
+  "C.pdb"
+  "C.exe"
+  "C.exe.manifest"
+  "libC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
